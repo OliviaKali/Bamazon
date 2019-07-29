bc59c78796733b8f0bc357ac744718db25cae1c6
OKali <o.kalinowski27@gmail.com>
2019-07-29 10:12:51 -0400

    Changed display from array to table

--- bamazon.sql
+++ bamazon.sql
@@ -9,38 +9,38 @@ CREATE TABLE products(
   product_name VARCHAR(45) NOT NULL,
   department_name VARCHAR(45) NOT NULL,
   price INTEGER(10) NOT NULL,
-  stock_quantity INTEGER (10) NULL,
+  stock_quantity INTEGER (10) NOT NULL,
   PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("laptop", "Technologies", 1500, 1);
+VALUES ("laptop", "Technology", 1500, 1);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("rocking chair", "Furniture", 300, 3);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ();
+VALUES ("iPhone5", "Techngology", 200, 5);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ();
+VALUES ("Psychology Textbook", "Books", 150, 10);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ();
+VALUES ("Used Piano", "Music", 3000, 1);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ();
+VALUES ("Gameboy with Pokemon Silver Game", "Miscallenous", 500, 5);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ();
+VALUES ("Pride and Prejudice", "Books", 5, 50);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ();
+VALUES ("Vintage SuperMan Shirt", "Clothes", 15, 15);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ();
+VALUES ("TV", "Technology", 1000, 7);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ();
+VALUES ("Lighter from Prague", "Miscellaneous", 3, 10);
 
 SELECT * FROM products
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,70 +1,72 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+  // Your port; if not 3306
+  port: 3306,
 
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
 
-    // Your password
-    password: "password",
-    database: "bamazon"
+  // Your password
+  password: "password",
+  database: "bamazon"
 });
-;
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Welcome to Bamazon!")
-    display();
-    start();
-    connection.end();
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Welcome to Bamazon!");
+  display();
+  start();
+  connection.end();
 });
 
 function display() {
-    connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
-        // console.log(results);
-        var itemChoices = [];
-        for (var i = 0; i < results.length; i++) {
-            itemChoices.push(
-                "ID: " + results[i].item_id +
-                ", Product: " + results[i].product_name +
-                ", Price: " + results[i].price);
-        }
-        // return itemChoices;
-        console.log(itemChoices);
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    // console.log(results);
+    var table = new Table({
+      head: ["ID", "Product Name", "Price"],
+      colWidths: [10, 45, 10]
+    });
+
+    for (var i = 0; i < results.length; i++) {
+      table.push([
+        results[i].item_id,
+        results[i].product_name,
+        results[i].price
+      ]);
     }
-)}
+    console.log(table.toString());
+  });
+}
 
 function start() {
-connection.query("SELECT * FROM products", function(err, results) {
+  connection.query("SELECT * FROM products", function(err, results) {
     if (err) throw err;
-    inquirer
-        .prompt([
-            {
-                name: "productToBuy",
-                type: "number",
-                message: "What item would you like to purchase? Please provide the ID #."
-            },
-            {
-                name: "amount",
-                type: "number",
-                message: "How much ??? would you like to purchase " 
-                //`{results[i].product_name}` 
-            }
-        ]).then(function (answer) {
-            var chosenItem = [];
-            for (var i = 0; i < results.length; i++) {
-               if (results[i].item_id === answer.proudctToBuy) {
-                   chosenItem = results[i].product_name
-               }
-            }
-            if (chosenItem)
-        })
+    inquirer.prompt([
+      {
+        name: "productToBuy",
+        type: "number",
+        message:
+          "What item would you like to purchase? Please provide the ID #."
+      },
+      {
+        name: "amount",
+        type: "number",
+        message: "How much would you like to purchase?"
+      }
+    ]);
+    // .then(function (answer) {
+    //     var chosenItem = [];
+    //     for (var i = 0; i < results.length; i++) {
+    //        if (results[i].item_id === answer.proudctToBuy) {
+    //            chosenItem = results[i].product_name
+    //        }
+    //     }
+    //     if (chosenItem)
+    // })
+  });
 }
-)}
-
